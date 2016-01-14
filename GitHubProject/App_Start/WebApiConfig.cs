749d1256973c2bb2eeda922c75804957582f4bef
Tanvir-Emedia <tanvir.emedia@outlook.com>
2016-01-14 11:08:01 +0530

    updates

--- GitHubProject/App_Start/WebApiConfig.cs
+++ GitHubProject/App_Start/WebApiConfig.cs
@@ -10,15 +10,15 @@ namespace GitHubProject
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
-
             // Web API routes
             config.MapHttpAttributeRoutes();
-
+            
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "api/{controller}/{action}/{id}",
                 defaults: new { id = RouteParameter.Optional }
-            );
+            );   
+
         }
     }
 }
